522223724522ee88f21730b3dc96fc9f633c0d5e
Farhan Yousaf <farhanyousaf1996@gmail.com>
2023-02-15 12:09:46 -0500

    Added pkgs

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:roomtemppro/DashBoard/DashBoard.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarIconBrightness: Brightness.dark,
+  ));
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sleek Circular Slider',
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DashBoard(),
+      title: 'Room Temp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color(0xff867cef),
+        backgroundColor: Color(0xfff0f0f0),
+        disabledColor: Color(0xffededed),
+        colorScheme: ColorScheme.fromSwatch(accentColor: Color(0xffaf92ea)),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const DashBoard(),
     );
   }
 }
